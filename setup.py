d1b46c182ec56c08cef5e1a0ecf849fafd1dff0f
Fabio Todaro <ft@ovalmoney.com>
2020-03-27 21:20:37 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Advanced Python Logger",
     long_description=make_long_description(),
     long_description_content_type="text/markdown",
-    version="0.3.0",
+    version="0.4.0",
     author="Fabio Todaro",
     license="MIT",
     author_email="ft@ovalmoney.com",
